cbcd8e8695d7baf5348e6f803a1049fce320b127
AbidShazid3 <abidshazid3@gmail.com>
2024-02-18 21:26:11 +0600

    added all function

--- js script/ticket.js
+++ js script/ticket.js
@@ -1,4 +1,29 @@
-function buyTickets(){
-    const Element = document.getElementById('show-section');
-    Element.document.addEventListener('scroll', function(event){});
+document.getElementById('scroll-button').addEventListener('click', function () {
+    const element = document.getElementById('show-section');
+    const mainElement = element.offsetTop;
+
+    window.scrollTo({
+        top: mainElement,
+        behavior: 'smooth',
+    })
+});
+
+let seatPrice= 550;
+const seatsSelection = document.querySelectorAll('.kbd');
+for(const seat of seatsSelection){
+    seat.addEventListener('click', function(){
+        const seatNumber = seat.querySelector('span');
+        const seatNumberText = seatNumber.innerText;
+        console.log(seatNumberText);
+        // to add the seat number text to the new p
+        const seatContainer = document.getElementById('seat-container');
+        const p = document.createElement('p');
+        p.innerText = seatNumberText;
+        seatContainer.appendChild(p);
+
+
+        // get the price
+        
+        
+    })
 }
